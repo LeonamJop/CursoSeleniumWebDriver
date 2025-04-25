18c9ad2ca3307867a03755b8b3920e540c004bb6
Leonam Silva <leonam.murilo@gmail.com>
2025-04-25 17:20:09 -0300

    Espera implicita

--- CursoSelenium3/src/main/java/TesteSincronismo.java
+++ CursoSelenium3/src/main/java/TesteSincronismo.java
@@ -1,3 +1,5 @@
+import java.util.concurrent.TimeUnit;
+
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
@@ -13,6 +15,7 @@ public class TesteSincronismo {
 	public void inicializa() {
 		driver = new ChromeDriver();
 		driver.manage().window().setSize(new Dimension(1200, 765));
+		//driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
 		driver.get("file:///" + System.getProperty("user.dir") + "/src/main/resources/componentes.html");
 		dsl = new DSL(driver);
 	}
@@ -23,11 +26,23 @@ public class TesteSincronismo {
 	}
 	
 	@Test
-	public void deveInteragirComRespostaDemorada() throws InterruptedException {
+	public void deveUtilizarEsperaFixa() throws InterruptedException {
 		dsl.clicaBotao("buttonDelay");
 		
 		Thread.sleep(5000);
 		
 		dsl.escreve("novoCampo", "Deu certo?");
+	}
+	
+	@SuppressWarnings("deprecation")
+	@Test
+	public void deveUtilizarEsperaImplicita() throws InterruptedException {
+		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+		
+		dsl.clicaBotao("buttonDelay");
+		
+		dsl.escreve("novoCampo", "Deu certo?");
+		
+		driver.manage().timeouts().implicitlyWait(0, TimeUnit.SECONDS);
 	}
 }
